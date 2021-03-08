93e77d11e43c1dedf166799bf95490ea9116ca35
Dharmesh Daula <ddaula@outlook.com>
2021-03-08 11:50:44 +0530

    DB Connectivity

--- App.py
+++ App.py
@@ -1,10 +1,36 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,redirect,url_for
+import psycopg2
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="postgres",
+    user="postgres",
+    password="Raptor_85")
 
 app=Flask(__name__)
+
+app.config['host']="localhost"
+app.config['database']="postgres"
+app.config['user']="postgres"
+app.config['password']="Raptor_85"
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/insert',methods=['POST'])
+def insert():
+    if request.method=="POST":
+        name=request.form['name']
+        email=request.form['email']
+        phone=request.form['phone']
+
+        cur = conn.cursor()
+        cur.execute("insert into students (ename,email,phone) VALUES (%s,%s,%s)",(name,email,phone))
+        conn.commit()
+        return redirect(url_for('index'))
+
+
 
 if __name__ == "__main__":
     app.run(port=5006,debug=True)
